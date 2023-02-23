f894d1809408d5723bf2e80e01f36ad33d283f8f
virbas@gmail.com <virbas@gmail.com>
2023-02-23 17:55:05 +0100

    initial commit for image-tracking

---
+++ examples/image-tracking/js/index.js
@@ -0,0 +1,24 @@
+/**
+ * /!\ This file is auto-generated.
+ *
+ * This is the entry point of your standalone application.
+ *
+ * You should register the component you need here, e.g.,
+ *
+ * ```
+ * import { MyComponent } from './my-component.js';
+ *
+ * WL.registerComponent(MyComponent);
+ * ```
+ *
+ * The `wle:auto-imports:start` and `wle:auto-imports:end` comments are
+ * used by the editor as a target for the import list.
+ */
+
+/* wle:auto-imports:start */
+import '@wonderlandengine/components';
+/* wle:auto-imports:end */
+
+import '../../common-components';
+
+import './image-tracker'
