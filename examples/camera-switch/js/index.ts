0d1ad2c61d51075ac08d5e3688cf13aee041c128
Linas Ozeraitis <virbas@gmail.com>
2023-03-09 11:42:42 +0100

    project setup

---
+++ examples/camera-switch/js/index.ts
@@ -0,0 +1,9 @@
+import '@wonderlandengine/components';
+
+import { ARFaceTrackingCamera } from '../../../src/components/AR/cameras/AR-face-tracking-camera';
+import { ARImageTrackingCamera } from '../../../src/components/AR/cameras/AR-image-tracking-camera';
+import { ARSLAMCamera } from '../../../src/components/AR/cameras/AR-SLAM-camera';
+
+WL.registerComponent(ARFaceTrackingCamera);
+WL.registerComponent(ARImageTrackingCamera);
+WL.registerComponent(ARSLAMCamera);
