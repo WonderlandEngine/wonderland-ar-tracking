7843f8afe32377afef963d15acd1a92631a5960b
Linas Ozeraitis <virbas@gmail.com>
2023-05-10 10:38:49 +0200

    update hit-test-location to use .active prop instead of scaling

--- examples/SLAM-camera/js/hit-test-location-xr8.ts
+++ examples/SLAM-camera/js/hit-test-location-xr8.ts
@@ -7,7 +7,7 @@
  * In case of intersection, positions the this.object at the intersection point
  */
 
-import {Component, Object as WLEObject} from '@wonderlandengine/api';
+import {Component, MeshComponent, Object as WLEObject} from '@wonderlandengine/api';
 import {property} from '@wonderlandengine/api/decorators.js';
 
 import {vec3} from 'gl-matrix';
@@ -29,39 +29,48 @@ export class HitTestLocationXR8 extends Component {
     private _intersectionVec3 = vec3.create();
     private _tmpWorldPosition = vec3.create();
 
-    init() {
+    private mesh!: MeshComponent;
+
+    start() {
         ARSession.getSessionForEngine(this.engine).onSessionStart.add(this.onSessionStart);
         ARSession.getSessionForEngine(this.engine).onSessionEnd.add(this.onSessionEnd);
+
+        this.mesh = this.object.getComponent<MeshComponent>(MeshComponent)!;
+        /**
+         * Hide the mesh until the hitpoint is found
+         */
+        this.mesh.active = false;
+
+        /** Stop update loop until the hitpoint is found */
+        this.active = false;
     }
 
     update() {
-        if (this._tracking) {
-            this.camera.getForwardWorld(this._camForward);
-            /* Intersect with origin XY plane. We always intersect if camera facing downwards */
-            if (this._camForward[1] < 0) {
-                this.camera.getPositionWorld(this._tmpWorldPosition);
-                const t = -this._tmpWorldPosition[1] / this._camForward[1];
-                vec3.add(
-                    this._intersectionVec3,
-                    this._tmpWorldPosition,
-                    vec3.scale(this._intersectionVec3, this._camForward, t)
-                );
-                this.object.setPositionWorld(this._intersectionVec3);
-            }
+        this.camera.getForwardWorld(this._camForward);
+        /* Intersect with origin XY plane. We always intersect if camera facing downwards */
+        if (this._camForward[1] < 0) {
+            this.camera.getPositionWorld(this._tmpWorldPosition);
+            const t = -this._tmpWorldPosition[1] / this._camForward[1];
+            vec3.add(
+                this._intersectionVec3,
+                this._tmpWorldPosition,
+                vec3.scale(this._intersectionVec3, this._camForward, t)
+            );
+            this.object.setPositionWorld(this._intersectionVec3);
         }
     }
 
     onSessionStart = (provider: ARProvider) => {
         if (provider instanceof XR8Provider) {
-            this.object.setScalingWorld([1, 1, 1]);
-            this._tracking = true;
+            this.active = true;
+            this.mesh.active = true;
         }
     };
 
     onSessionEnd = (provider: ARProvider) => {
         if (provider instanceof XR8Provider) {
-            this._tracking = false;
-            this.object.setScalingWorld([0, 0, 0]);
+            this.active = false;
+            this.mesh.active = false;
         }
     };
 }
